69c23c3d2b2cbda855610dbd202b4403c22718ed
lovtam <lovtamyan@gmail.com>
2015-12-20 22:01:52 +0800

    add register activity & use blank activity instead of sliding activity for the bug of baidu API

---
+++ app/src/main/java/com/example/hosin/bikesummon/RegitsterActivity.java
@@ -0,0 +1,273 @@
+package com.example.hosin.bikesummon;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RegitsterActivity extends AppCompatActivity {
+
+    private View mProgressView;
+    private View mRegisterView;
+
+    private EditText email;
+    private EditText phone;
+    private EditText password;
+    private EditText nickname;
+    private Button register;
+    private RadioGroup whois;
+
+    private UserRegisterTask mAuthTask=null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regitster);
+        mRegisterView=findViewById(R.id.register_form);
+        mProgressView = findViewById(R.id.register_progress);
+        whois =(RadioGroup) findViewById(R.id.radioGroup);
+        email=(EditText)findViewById(R.id.registerEmail);
+        phone=(EditText)findViewById(R.id.registerPhone);
+        password=(EditText)findViewById(R.id.registerPassword);
+        nickname=(EditText)findViewById(R.id.nickname);
+        register=(Button)findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptRegister();
+            }
+        });
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mRegisterView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mRegisterView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mRegisterView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mRegisterView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    private void attemptRegister() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        email.setError(null);
+        password.setError(null);
+        nickname.setError(null);
+        phone.setError(null);
+
+        // Store values at the time of the login attempt.
+        String mEmail = email.getText().toString();
+        String mPassword = password.getText().toString();
+        String mNickName = nickname.getText().toString();
+        String mPhone = phone.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(mPassword) && !isPasswordValid(mPassword)) {
+            password.setError(getString(R.string.error_invalid_password));
+            focusView = password;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(mEmail)) {
+            email.setError(getString(R.string.error_field_required));
+            focusView = email;
+            cancel = true;
+        } else if (!isEmailValid(mEmail)) {
+            email.setError(getString(R.string.error_invalid_email));
+            focusView = email;
+            cancel = true;
+        }
+
+        //Check for a valid phone
+        if(TextUtils.isEmpty(mPhone)){
+            phone.setError(getString(R.string.error_field_required));
+            focusView = phone;
+            cancel = true;
+        }else if(!isPhoneValid(mPhone)){
+            phone.setError("invalid phone number");
+            focusView=phone;
+            cancel=true;
+        }
+
+        //Check for a valid nickname
+        if(TextUtils.isEmpty(mNickName)){
+            nickname.setError(getString(R.string.error_field_required));
+            focusView=nickname;
+            cancel=true;
+        }else if(!isNicknameValid(mNickName)){
+            nickname.setError("invalid nickname");
+            focusView=nickname;
+            cancel=true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            if(whois.getCheckedRadioButtonId()==R.id.registerCustom){
+                mAuthTask=new UserRegisterTask(mEmail,mPhone,mNickName,mPassword,true);
+            }else{
+                mAuthTask=new UserRegisterTask(mEmail,mPhone,mNickName,mPassword,false);
+            }
+
+            mAuthTask.execute((Void) null);
+        }
+    }
+
+    private boolean isNicknameValid(String mNickName) {
+        //TODO: More judgement on nickname
+            return mNickName.length() > 4;
+    }
+
+    private boolean isPhoneValid(String mPhone) {
+        //TODO: More judgement on phone
+        return mPhone.length()==11;
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: More judgement on Email
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: More judgement on password
+        return password.length() > 4;
+    }
+
+    public class UserRegisterTask extends AsyncTask<Void,Void,Integer>{
+
+        private Boolean isCustom;
+        private String email;
+        private String nickname;
+        private String password;
+        private String phone;
+        private final String url=null; //TODO: wait for url
+        private HttpURLConnection connection=null;
+
+        UserRegisterTask(String email,String phone,String nickname,String password,Boolean isCustom){
+            this.email=email;
+            this.phone=phone;
+            this.nickname=nickname;
+            this.password=password;
+            this.isCustom=isCustom;
+        }
+        @Override
+        protected Integer doInBackground(Void... params) {
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+
+                /*
+                connection = (HttpURLConnection) ((new URL(url.toString()).openConnection()));
+                connection.setRequestMethod("POST");
+                connection.setConnectTimeout(8000);
+                connection.setReadTimeout(8000);
+
+                //upload data
+                DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("phone",phone);
+                jsonObject.put("nickname",nickname);
+                jsonObject.put("email", email);
+                //TODO: encrypt the password
+                jsonObject.put("passwd", password);
+                if (isCustom) {
+                    jsonObject.put("type", "custom");
+                } else {
+                    jsonObject.put("type", "driver");
+                }
+                out.writeBytes(jsonObject.toString());
+
+                //get data from server
+                InputStream in = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                JSONObject res = new JSONObject(response.toString());
+                return res.getInt("userID");*/
+                return 2;
+            } catch (Exception e) {
+                return -1;
+            }
+        }
+
+        protected void onPostExecute(final Integer i) {
+            if(i>0){
+                if(isCustom){
+                    Intent intent=new Intent(RegitsterActivity.this,TestActivity.class);
+                    intent.putExtra("userID",i);
+                    startActivity(intent);
+                }else {
+
+                }
+
+            }else{
+
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask=null;
+            super.onCancelled();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hosin/bikesummon/TestActivity.java
@@ -0,0 +1,93 @@
+package com.example.hosin.bikesummon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+public class TestActivity extends AppCompatActivity {
+    private MapView mapView=null;
+    private LocationClient locationClient=null;
+    boolean isFirst=true;
+
+    private BDLocationListener locationListener= new BDLocationListener() {
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            //Log.d("test", String.valueOf(bdLocation.getLatitude()));
+           // Log.d("test", String.valueOf(bdLocation.getLongitude()));
+            if(isFirst){
+                LatLng latLng=new LatLng(bdLocation.getLatitude(),bdLocation.getLongitude());
+                MapStatusUpdate update= MapStatusUpdateFactory.newLatLng(latLng);
+                mapView.getMap().setMapStatus(update);//locate where you are on the map
+                isFirst=false;
+            }
+            MyLocationData data=new MyLocationData.Builder().accuracy(100).latitude(bdLocation.getLatitude()).longitude(bdLocation.getLongitude()).build();
+            mapView.getMap().setMyLocationData(data);
+            mapView.getMap().setMyLocationEnabled(true);
+            mapView.getMap().setMyLocationConfigeration(new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL, true, null));
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SDKInitializer.initialize(getApplicationContext());
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        //Location
+        locationClient=new LocationClient(getApplicationContext());
+        locationClient.registerLocationListener(locationListener);
+        initLocation();
+        locationClient.start();
+
+
+        mapView=(MapView) findViewById(R.id.testMapview);
+        mapView.getMap().setMyLocationEnabled(true);
+        mapView.getMap().setMyLocationConfigeration(new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL,true,null));
+    }
+
+    private void initLocation() {
+        LocationClientOption option=new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        option.setScanSpan(1000);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认false，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
+        locationClient.setLocOption(option);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+}
